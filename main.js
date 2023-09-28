5827eb0949a7c50042b7678435ef933ceb04e93e
giovannaspadilha <111082616+giovannaspadilha@users.noreply.github.com>
2023-09-28 08:02:35 -0300

    ;

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-function TocaSomPom () {
-    document.querySelector('som-tecla_pom') . play ();
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom') . play ();
 }
 
 document.querySelector('.tecla_pom');
