67328f460dd437169ef5957e3d48ea86d78d8424
giovannaspadilha <111082616+giovannaspadilha@users.noreply.github.com>
2023-09-28 08:24:12 -0300

    jkhv

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom') . play ();
+fuction tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
 }
-
-document.querySelector('.tecla_pom');
+}
